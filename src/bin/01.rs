a150d23e2eda41e2d79f06bb8be69a548cf9db0f
Justin Phu <me@justinphu.dev>
2022-12-01 16:10:06 +1100

    ✨ add solution to day 1
    Summary:
    
    Test Plan:

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,9 +1,37 @@
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut calories = vec![];
+    let mut counter = 0;
+    for line in input.lines() {
+        if !line.is_empty() {
+            counter += line.parse::<u32>().unwrap();
+        } else {
+            calories.push(counter);
+            counter = 0;
+        }
+    }
+
+    calories.push(counter);
+
+    calories.sort_by(|a, b| b.cmp(a));
+    Some(calories[0])
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut calories = vec![];
+    let mut counter = 0;
+    for line in input.lines() {
+        if !line.is_empty() {
+            counter += line.parse::<u32>().unwrap();
+        } else {
+            calories.push(counter);
+            counter = 0;
+        }
+    }
+
+    calories.push(counter);
+
+    calories.sort_by(|a, b| b.cmp(a));
+    Some(calories[0] + calories[1] + calories[2])
 }
 
 fn main() {
@@ -19,12 +47,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(70296));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(205381));
     }
 }
